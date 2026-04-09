335184f6bcd3fb0044ba64768f5fd3b2add5ada6
Benjamin Martinez <santiipettinati2@gmail.com>
2026-04-09 19:54:33 -0300

    Creo que no anda

--- Veterinaria.cs
+++ Veterinaria.cs
@@ -1,6 +1,41 @@
-﻿namespace Prog1_Practica_Git_PettinatiMartinez;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+namespace Prog1_Practica_Git_PettinatiMartinez;
 
-public class Veterinaria
-{
+    public class Veterinaria
+    {
+        private List <Mascota> Mascotas = new List<Mascota>();
 
-}
+        public Veterinaria()
+    {
+        Mascotas = new List<Mascota>();
+    }
+
+        public Mascota RegistrarMascota(Mascota mascota)
+    {
+        Mascotas.Add(mascota);
+
+        return mascota;
+    }
+
+        public string MostrarTodas()
+    {
+        
+
+        if (Mascotas.Count == 0)
+        {
+            return "No hay";
+        } else
+        {
+            foreach (var mascota in Mascotas)
+            {
+                string si = Convert.ToString(mascota.ObtenerInfo);
+                return $"Mascota: {si}";
+            }
+        }
+    }
+
+    }
