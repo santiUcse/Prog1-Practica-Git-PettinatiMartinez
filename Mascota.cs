32d7ee618dff6650642f343ea2c5e3223e5741f5
Benjamin Martinez <santiipettinati2@gmail.com>
2026-04-09 19:37:10 -0300

    agregue mascota y algo mas

---
+++ Mascota.cs
@@ -0,0 +1,24 @@
+﻿namespace Prog1_Practica_Git_PettinatiMartinez;
+
+public class Mascota
+{
+    public string Nombre {get;set;}
+    public string Especie{get;set;}
+    public int Edad{get;set;}
+    public Dueno Dueno {get;set;}
+
+        public Mascota(string nombre, string especie, int edad, Dueno dueno)
+    {
+        Nombre = nombre;
+        Especie = especie;
+        Edad = edad;
+        Dueno = dueno;
+    }
+
+    public string ObtenerInfo()
+    {
+        return $"{Nombre} | {Especie} | {Edad} años | Dueño: {Dueno.Nombre}";
+    }
+}
+
+
